f253359cc5600c60f4c4425c557793c298e55122
Yastin <yastin@gmail.com>
2026-06-17 11:28:41 +0800

    Fix settings module name

--- create_superuser.py
+++ create_superuser.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mvnp_final.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
